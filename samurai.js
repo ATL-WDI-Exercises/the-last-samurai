8250176f3db8ee384a788a2cf9bbb0c47d6a4977
Mike Hopper <drmikeh@gmail.com>
2016-01-25 16:13:59 -0500

    Updated solution.

--- samurai.js
+++ samurai.js
@@ -1,17 +1,28 @@
 // The Last Samurai
 
+/*
 var samurai = [];
 for (var i = 1; i <= 100; i++) {
   samurai.push(i);
 }
+*/
 
-var killer = 0;
-while(samurai.length > 1) {
-  var toBeKilled = (killer + 1) % samurai.length;
-  console.log('%s, %s ==> %s about to kill %s : %s',
-              killer, toBeKilled, samurai[killer], samurai[toBeKilled], samurai.length);
-  samurai.splice(toBeKilled, 1);
-  killer = toBeKilled % samurai.length;   // because we spliced out the dead guy.
+function lastSamurai(numSamurai) {
+
+  var samurai = Array.from(new Array(numSamurai).keys(), x => x + 1);
+
+  var killer = 0;
+  while(samurai.length > 1) {
+    var toBeKilled = (killer + 1) % samurai.length;
+    console.log('(%s) %s is about to kill (%s) %s leaving remaining = %s',
+                killer, samurai[killer], toBeKilled, samurai[toBeKilled], samurai.length - 1);
+    samurai.splice(toBeKilled, 1);
+    killer = toBeKilled % samurai.length;   // because we spliced out the dead guy.
+  }
+
+  return samurai[0];
 }
 
-console.log('Last one standing:', samurai);
+console.log('For 5 Samurai, the last one standing is:', lastSamurai(5));
+console.log();
+console.log('For 100 Samurai, the last one standing is:', lastSamurai(100));
